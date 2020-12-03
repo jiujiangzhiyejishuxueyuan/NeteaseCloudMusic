9287e89fa437785fd31e2d3353d4703b94b65782
willy peng <49059302+jiujiangzhiyejishuxueyuan@users.noreply.github.com>
2020-12-03 21:52:06 +0800

    添加载入动画

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
         //以上的ip和端口是我们本机的;下面为需要跨域的
         proxy: {//配置跨域
             '/api': {
-                target: '',//后台接口
+                target: 'http://47.106.222.173:3000/',//后台接口
                 ws: true,
                 changOrigin: true,//允许跨域
                 pathRewrite: {

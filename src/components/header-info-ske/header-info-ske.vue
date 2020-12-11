9b75e50aa89d53e49131fb6a5436f75106c570cc
willy peng <49059302+jiujiangzhiyejishuxueyuan@users.noreply.github.com>
2020-12-11 20:47:17 +0800

    修改骨架屏样式

--- src/components/header-info-ske/header-info-ske.vue
+++ src/components/header-info-ske/header-info-ske.vue
@@ -1,73 +1,76 @@
 <template>
-    <div id="header-info-ske" :class="{s:s}">
-        <div class="container flex info-inner">
-            <div class="cover header-info-img"></div>
-            <div class="info">
-                <div class="name"></div>
-                <div class="creator"></div>
-                <div class="time"></div>
-                <div class="desc"></div>
-                <div class="control flex" v-if="!s">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-
+  <div id="header-info-ske" :class="{s:s}">
+    <div class="container flex info-inner my-card">
+      <div class="cover header-info-img"></div>
+      <div class="info">
+        <div class="name"></div>
+        <div class="creator"></div>
+        <div class="time"></div>
+        <div class="desc"></div>
+        <div class="control flex" v-if="!s">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
+      </div>
+
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "header-info-ske",
-        props: {
-            s: Boolean
-        }
-    }
+export default {
+  name: "header-info-ske",
+  props: {
+    s: Boolean
+  }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-    #header-info-ske
-        padding 50px 0
+#header-info-ske
+  padding 50px 0
 
-        &.s
-            padding 0
+  &.s
+    padding 0
 
-        .cover, .name, .creator, .desc, .time, .control div
-            animation ske .8s linear infinite alternate
+  &.my-card
+    padding 20px
 
-        .info
-            width 75%
-            margin-left 30px
-            position relative
+  .cover, .name, .creator, .desc, .time, .control div
+    animation ske .8s linear infinite alternate
 
-            .control
-                position absolute
-                bottom 20px
+  .info
+    width 75%
+    margin-left 30px
+    position relative
 
-                div
-                    width 130px
-                    margin-right 30px
-                    height 40px
-                    border-radius 20px
+    .control
+      position absolute
+      bottom 20px
 
-            div
-                height 22px
+      div
+        width 130px
+        margin-right 30px
+        height 40px
+        border-radius 20px
 
-            .name
-                width 250px
-                height 40px
+    div
+      height 22px
 
-            .creator
-                margin-top 30px
-                width 120px
+    .name
+      width 250px
+      height 40px
 
-            .time
-                margin-top 10px
-                width 130px
+    .creator
+      margin-top 30px
+      width 120px
 
-            .desc
-                margin-top 10px
-                width 50%
+    .time
+      margin-top 10px
+      width 130px
+
+    .desc
+      margin-top 10px
+      width 50%
 </style>
